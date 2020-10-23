f5123712aded9ec8d06b5138cfbbb89f8b1956db
Dmitriy <demon-x.88@mail.ru>
2020-10-24 00:01:14 +0300

    выполнил задание

--- 3/assets/scripts/app.js
+++ 3/assets/scripts/app.js
@@ -1,45 +1,65 @@
-
-
 const monsterHP = 100;
 const playerHP = 100;
 
-
+let data = {};
 
 
 attackBtn.onclick = function lowDamage() {
     const hitMonster = monsterHP - applyMonsterDamage(10);
     const hitPlayer = playerHP - applyPlayerDamage(14);
     if (monsterHealthBar.value === 0) {
-        alert ("You win");
-        resetGame(monsterHP);
+        if(confirm("You win")){        
+            alert('сыграем еще раз?')
+            resetGame(100);
+        } else {
+            alert("До скорого!");
+        }
+        data['You win'] = 'Вы выиграли!!!';        
     } else if (playerHealthBar.value === 0) {
-        alert ("WASTED");
-        resetGame(playerHP);
-    }
+        if(confirm("WASTED")){        
+            alert('сыграем еще раз?')
+            resetGame(100);
+        } else {
+            alert("До скорого!");
+        }
+        data['WASTED'] = 'Вы проиграли!!!';
+        }
+    data[100 - monsterHealthBar.value] = 'Урон Монстру';
+    data[100 - playerHealthBar.value] = 'Урон Игроку';
     
-    console.log (monsterHealthBar.value);
-    console.log (playerHealthBar.value);
 }
 
 strongAttackBtn.onclick = function damageHigh() {
     const hitMonster = monsterHP - applyMonsterDamage(30);
     const hitPlayer = playerHP - applyPlayerDamage(14);
     if (monsterHealthBar.value === 0) {
-        alert ("You win");
-        resetGame(100);
+        if(confirm("You win")){        
+            alert('сыграем еще раз?')
+            resetGame(100);
+        } else {
+            alert("До скорого!");
+        }
+        data['You win'] = 'Вы выиграли!!!';        
     } else if (playerHealthBar.value === 0) {
-        alert ("WASTED");
-        resetGame(100);
-    }
+        if(confirm("WASTED")){        
+            alert('сыграем еще раз?')
+            resetGame(100);
+        } else {
+            alert("До скорого!");
+        }
+        data['WASTED'] = 'Вы проиграли!!!';
+        }
+    data[100 - monsterHealthBar.value] = 'Урон Монстру';
+    data[100 - playerHealthBar.value] = 'Урон Игроку';
 }
 healBtn.onclick = function useHealth() {
     removeBonusLife();    
-    increasePlayerHealth(100);
-    playerHP - applyPlayerDamage(25);
-    
+    increasePlayerHealth(playerHP);
+    let newHP = playerHP - applyPlayerDamage(25); 
+    data[playerHP - newHP] = 'Использование доп жизни';   
 }
 
 logBtn.onclick = function downLog() {
-    alert("выгрузка лога")
+    console.log(data);
 }
 
